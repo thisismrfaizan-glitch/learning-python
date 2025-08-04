1c171d1a97bca2f349f4c47c0a96cb827e51e6b6
Shaikh Faizan <thisismrfaizan@gmail.com>
2025-08-04 22:48:12 +0530

    Add files via upload
    comeback code with the revision of the learnings and basics.

---
+++ first _code_of_revision.py
@@ -0,0 +1,40 @@
+print("hello word")
+
+#module
+import pyjokes
+print(pyjokes.get_joke())
+
+# This code is a simple Python script that prints "hello word" and then uses the pyjokes library to print a random joke. 
+
+# this is comment that i used to  mention that will not be executed
+
+# getting input from user and #printing it
+name = input("enter your name: ")
+print ("welcome back dear lets get back to the work ", name)
+#sum of input that user give and substractin,multiplication,division etc.
+#sum
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+print("the sum of the two numbers is: ", a+b)
+
+#substraction 
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+
+print("the substraction of the two numbers is: ", a-b)
+
+#multiplication
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+print("the multiplication of the two numbers is: ", a * b)
+
+#division
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+print("the division of the two numbers is: ", a/b)
+
+#finding the class or type of the input
+a = input()
+print(type(a))
+
+# much more things learnt so far will create more folder sequence tommorow today i just came back from the gap i tried to be back on track
